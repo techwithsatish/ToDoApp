0fcdd44f3a8fc53ad09b55c6f174132a5dbccb5c
techwithsatish <satish15.dev@gmail.com>
2025-04-26 21:04:25 +0530

    Centred the ToDoItems

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { dummyData } from './data/todos'
 import './index.css'
 
 function App() {
@@ -7,8 +8,17 @@ function App() {
   return (
     <main className='py-10 h-screen bg-black text-white'>
       <h1 className='font-bold text-3xl text-center'>
-        Hello
+        Your ToDos
       </h1>
+      <div className='max-w-lg mx-auto'>
+        <div className='space-y-2'>
+          {dummyData.map(todo => (
+            <p key={todo.id} className='text-lg'>
+              {todo.title}
+            </p>
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
---
+++ src/data/todos.ts
@@ -0,0 +1,21 @@
+import { Todo } from "../types/todo";
+
+
+
+export const dummyData: Todo[] = [
+    {
+    id: 1,
+    title: "Like This Video",
+    completed: false,
+    },
+    {
+    id: 2,
+    title: "Subscribe To Coding in Flow",
+    completed: false,
+    },
+    {
+    id: 3,
+    title: "Leave a Nice Comment",
+    completed: false,
+    },
+];
