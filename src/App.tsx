724bbd5e115a72cebf52d3a18fbf6e1f323aff3d
techwithsatish <satish15.dev@gmail.com>
2025-04-26 18:56:02 +0530

    initial setup is complete

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+
+  return (
+    <main>
+      <h1 className='text-8xl'>
+        Hello
+      </h1>
+    </main>
+  )
+}
+
+export default App
